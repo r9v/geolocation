051ed0bd2bd13e44f4580457eacec1c8fe976d1c
r9v <r9v>
2022-02-17 20:29:10 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  serverRuntimeConfig: {
+    secret: "JWTSecret",
+  },
+  publicRuntimeConfig: {
+    apiUrl:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/api"
+        : "http://localhost:3000/api",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
